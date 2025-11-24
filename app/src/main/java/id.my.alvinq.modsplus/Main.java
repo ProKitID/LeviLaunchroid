f99b74c30d956795e868a35b230b105e56efe9bc
AlvinQID <github@alvinq.my.id>
2025-11-24 16:45:47 +0700

    Add Main class with onLoad and onUnload methods

---
+++ app/src/main/java/id.my.alvinq.modsplus/Main.java
@@ -0,0 +1,10 @@
+package id.my.alvinq.modsplus;
+
+import android.content.Context;
+
+public class Main {
+  public static void onLoad(Context c) {
+  }
+  public static void onUnload(Context a) {
+  }
+}
